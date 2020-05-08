43b371f21dc034cd5cb567528fe3749ffae47e9e
Lucas Pereira <pereirapas123@gmail.com>
2020-05-08 19:01:13 -0300

    User insert

--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.workshopmongo.dto.UserDTO;
 import com.example.workshopmongo.entities.User;
 import com.example.workshopmongo.repositories.UserRepository;
 import com.example.workshopmongo.services.exceptions.ObjectNotFoundException;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
